0b4918a0cbe098f827fa15153fc52073c340e151
Leul-deg <94111436+Leul-deg@users.noreply.github.com>
2022-12-23 11:35:12 +0300

    Time: 2634 ms (74.43%), Space: 71.3 MB (6.60%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,18 +1,25 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        zero_loss , one_loss , more_loss = set() , set() , set()
+        maximum = 0
+        for l , w in matches:
+            maximum = max(maximum , l)
+            maximum = max(maximum , w)
+        count = [0 for i in range(maximum+1)]
+        seen = set()
         for winner , loser in matches:
-            if winner not in one_loss and winner not in more_loss:
-                zero_loss.add(winner)
-            if loser not in zero_loss and loser not in one_loss and loser not in more_loss:
-                one_loss.add(loser)
-            elif loser in zero_loss:
-                zero_loss.remove(loser)
-                one_loss.add(loser)
-            elif loser in one_loss:
-                one_loss.remove(loser)
-                more_loss.add(loser)
-        ans = [sorted(list(zero_loss)) , sorted(list(one_loss))]
+            seen.add(loser)
+            seen.add(winner)
+            count[loser] += 1
+        
+        ans = [[] , []]
+        
+        for idx in range(len(count)):
+            if idx in seen:
+                if count[idx] == 0:
+                    ans[0].append(idx)
+                elif count[idx] == 1:
+                    ans[1].append(idx)
         return ans
+                    
         
                 
