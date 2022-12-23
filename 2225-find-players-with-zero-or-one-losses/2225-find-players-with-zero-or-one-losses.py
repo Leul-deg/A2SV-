6b0050d48042a9e36e124e003b24a27176ca96f8
Leul-deg <94111436+Leul-deg@users.noreply.github.com>
2022-12-23 11:10:04 +0300

    Time: 3037 ms (68.70%), Space: 68.8 MB (60.42%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -1,20 +1,18 @@
 class Solution:
     def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
-        hashmap = {}
-        for winner, loser in matches:
-            win_val = hashmap.get(winner , [0,0])
-            win_val[0] += 1
-            hashmap[winner] = win_val
-            los_val = hashmap.get(loser , [0,0])
-            los_val[1] += 1
-            hashmap[loser] = los_val
-        ans = [[] , []]
-        for key in hashmap:
-            win , lose = hashmap[key]
-            if lose == 0:
-                ans[0].append(key)
-            elif lose == 1:
-                ans[1].append(key)
-        ans[0].sort()
-        ans[1].sort()
+        zero_loss , one_loss , more_loss = set() , set() , set()
+        for winner , loser in matches:
+            if winner not in one_loss and winner not in more_loss:
+                zero_loss.add(winner)
+            if loser not in zero_loss and loser not in one_loss and loser not in more_loss:
+                one_loss.add(loser)
+            elif loser in zero_loss:
+                zero_loss.remove(loser)
+                one_loss.add(loser)
+            elif loser in one_loss:
+                one_loss.remove(loser)
+                more_loss.add(loser)
+        ans = [sorted(list(zero_loss)) , sorted(list(one_loss))]
         return ans
+        
+                
