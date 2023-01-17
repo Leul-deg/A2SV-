be16a3a031251e96ebedb83efd28bf5f6125e936
Leul-deg <94111436+Leul-deg@users.noreply.github.com>
2023-01-17 19:54:13 +0300

    Time: 883 ms (8.33%), Space: 15.9 MB (8.95%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,18 +1,12 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
         hashmap = {heights[idx] : names[idx] for idx in range(len(names))}
-        # def bubble_sort(array):
-        #     for i in range(len(array)):
-        #         for j in range(len(array)):
-        #             if j+1 < len(array) and array[j] < array[j+1]:
-        #                 array[j] , array[j+1] = array[j+1] , array[j]
-        # bubble_sort(heights)
-        def insertion_sort(array):
-            for idx in range(len(array)):
-                cur = idx
-                while cur > 0 and array[cur-1] < array[cur]:
-                    array[cur-1] , array[cur]  = array[cur] , array[cur - 1]
-                    cur -= 1
-                    
-        insertion_sort(heights)
+        store =  [0] * (max(heights) + 1)
+        sorted_heights = []
+        for height in heights:
+            store[height] += 1
+        for idx in range(len(store)):
+            sorted_heights.extend([idx]*store[idx])
+        heights = sorted_heights[-1::-1]
+        
         return [hashmap[height] for height in heights]
