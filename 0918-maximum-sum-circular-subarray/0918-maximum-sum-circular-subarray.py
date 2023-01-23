1f2d8a5d8013f2b323f99cc72d5983757e3aa7ed
Leul-deg <94111436+Leul-deg@users.noreply.github.com>
2023-01-23 22:15:45 +0300

    Time: 570 ms (35.70%), Space: 18.9 MB (91.53%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -1,31 +1,17 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
         total = sum(nums)
-        forward = []
-        prefix = 0
+        prefix ,min_prefix , max_prefix = 0 , 0 , 0
+        res = float("-inf")
+        
         for num in nums:
-            forward.append(total - prefix)
             prefix += num
+            res = max(res , prefix - min_prefix)
+            res = max(res, total - prefix + num + max_prefix)
+            min_prefix , max_prefix = min(prefix,min_prefix) , max(max_prefix,prefix)
         
-        till_me_max = []
-        prefix = 0
-        for num in nums:
-            if till_me_max:
-                till_me_max.append(max(prefix , till_me_max[-1]))
-            else:
-                till_me_max.append(0)
-            prefix += num
-        
-        prefix = 0
-        min_prefix = 0
-        maximum = float("-inf")
-        for idx,num in enumerate(nums):
-            prefix += num
-            maximum= max(maximum , prefix - min_prefix)
-            maximum = max(maximum , till_me_max[idx] + forward[idx])
-            min_prefix = min(min_prefix , prefix)
-        
-        return maximum
+        return res
+            
             
             
             
