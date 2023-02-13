480fe6d95c5c033969d3e9a5a69102674d1c84a8
Leul-deg <94111436+Leul-deg@users.noreply.github.com>
2023-02-13 09:45:00 +0300

    Time: 38 ms (28.93%), Space: 13.9 MB (50.04%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        if low % 2 == 0:
+            low += 1
+        
+        if high % 2 == 0:
+            high -= 1
+        
+        if low > high:
+            return 0
+        
+        if low == high:
+            return 1
+        
+        return (high - low)//2 + 1
+        
+        
+        
