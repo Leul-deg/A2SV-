23e5ded148ad61bbaa0b64e9c40e847e2cfe92af
Leul-deg <94111436+Leul-deg@users.noreply.github.com>
2023-05-09 14:15:53 +0300

    added remove-stones-to-minimize-the-total.python

---
+++ remove-stones-to-minimize-the-total.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        heap = [-1*pile for pile in piles]
+        heapq.heapify(heap)
+        total = sum(piles)
+        while k and heap:
+            top = -1*heappop(heap)
+            a= math.floor(top/2)
+            top = top - a
+            total -= (a)
+            if top:
+                heappush(heap,-1*top)
+            k -= 1
+        return total
